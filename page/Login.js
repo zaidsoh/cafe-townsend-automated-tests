5ab66fa6c04e6f6290c253c2e3f0c44255784908
zaidsoh <57127906+zaidsoh@users.noreply.github.com>
2022-07-25 17:41:32 +0500

    Update Login.js

--- page/Login.js
+++ page/Login.js
@@ -1,38 +1,34 @@
 var Login = function () {
 
-	var Main = require ('../page/Main.js');
+	const Main = require ('../page/Main.js');
 
-	var username = element (by.model ('user.name'));
-	var password = element (by.model ('user.password'));
-	var loginButton = element (by.buttonText('Login'));
-	var loginError = element(by.xpath('//*[@id="login-form"]/fieldset/p[1]'));
+	const username = element (by.model ('user.name'));
+	const password = element (by.model ('user.password'));
+	const loginButton = element (by.buttonText('Login'));
+	const loginError = element(by.xpath('//*[@id="login-form"]/fieldset/p[1]'));
 
 
 	this.enterUsername = function (value) {
-
 		username.clear ();   
 		username.sendKeys (value);
-	};
+	}
+
 
 	this.enterPassword = function (value) {
-
         password.clear (); 
 		password.sendKeys (value);
 	};
 
 	this.clickLogin = function () {
-
 		loginButton.click();
 	};
 
 	this.verifyInvalid = function () {
-
 		expect (loginError.isPresent()).toBe(true);
 		expect (loginError.getText()).toEqual ('Invalid username or password!');
 	};
 
 	this.verifyValid = function () {
-
 		expect (Main.greetingMessage().isPresent()).toBe(true);
 		expect (Main.greetingMessage().getText()).toEqual('Hello Luke');
     };
